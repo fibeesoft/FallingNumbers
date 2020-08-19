b524620fa44d37c96b138640c64a91d76672b38b
pikademia <pikademia@gmail.com>
2020-08-19 15:53:22 +0200

    Spawn FallingNumbers with timer

--- Falling Numbers/Assets/FNSpawning.cs
+++ Falling Numbers/Assets/FNSpawning.cs
@@ -7,20 +7,31 @@ public class FNSpawning : MonoBehaviour
     public GameObject fnPrefab;
     public Canvas canvas;
     Vector2 startingPosition;
+    float timeToSpawn = 1f;
+    float timer = 0f;
     void Start()
     {
         startingPosition = new Vector2(0f, 60f);
-        SpawnFN();
     }
 
     void Update()
     {
-        
+        CountDown();
     }
 
     void SpawnFN()
     {
         GameObject fn = Instantiate(fnPrefab, canvas.transform);
         fn.GetComponent<RectTransform>().anchoredPosition = startingPosition;
+    }
+
+    void CountDown()
+    {
+        timer += Time.deltaTime;
+        if(timer > timeToSpawn)
+        {
+            SpawnFN();
+            timer = 0f;
+        }
     }
 }
