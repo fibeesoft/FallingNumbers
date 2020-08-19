d6014b12bb7e991528aa8c46020be54e8b6f931e
pikademia <pikademia@gmail.com>
2020-08-19 15:30:41 +0200

    Spawn a new FallingNumber on the scene

---
+++ Falling Numbers/Assets/FNSpawning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FNSpawning : MonoBehaviour
+{
+    public GameObject fnPrefab;
+    public Canvas canvas;
+    Vector2 startingPosition;
+    void Start()
+    {
+        startingPosition = new Vector2(0f, 60f);
+        SpawnFN();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnFN()
+    {
+        GameObject fn = Instantiate(fnPrefab, canvas.transform);
+        fn.GetComponent<RectTransform>().anchoredPosition = startingPosition;
+    }
+}
