d7f0262f05f7e4f19e5641bc51aa078794880ef9
pikademia <pikademia@gmail.com>
2020-08-19 11:04:16 +0200

    Move FallingNumber down

---
+++ Falling Numbers/Assets/FNMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FNMove : MonoBehaviour
+{
+    float fallingSpeed = 120f;
+    Vector2 moveDirection;
+    RectTransform rt;
+    void Start()
+    {
+        moveDirection = new Vector2(0f, -1f);
+        rt = GetComponent<RectTransform>();
+    }
+
+
+    void Update()
+    {
+        MoveDown();
+    }
+
+    void MoveDown()
+    {
+        rt.anchoredPosition += moveDirection * Time.deltaTime * fallingSpeed;
+    }
+}
