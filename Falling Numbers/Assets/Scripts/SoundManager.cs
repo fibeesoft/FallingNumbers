b3cfb3d53e6edc7deba404c6d91c63f08eb0a7e3
pikademia <pikademia@gmail.com>
2020-08-21 09:12:26 +0200

    Background sound

---
+++ Falling Numbers/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip[] backgroundSounds;
+    bool isSoundOn;
+    AudioSource audios;
+    void Start()
+    {
+        audios = GetComponent<AudioSource>();
+        isSoundOn = true;
+        ChooseBgSound(0);
+
+    }
+
+    void Update()
+    {
+        if(isSoundOn && !audios.isPlaying)
+        {
+            audios.Play();
+        }else if (!isSoundOn)
+        {
+            audios.Stop();
+        }
+    }
+
+    public void ChooseBgSound(int clipNumber)
+    {
+        audios.clip = backgroundSounds[clipNumber];
+    }
+
+    public void SwitchSoundsOnOff()
+    {
+        if (isSoundOn)
+        {
+            isSoundOn = false;
+        }
+        else
+        {
+            isSoundOn = true;
+        }
+    }
+}
